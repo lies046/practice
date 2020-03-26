ea6c480d0d06747d858e038f99cb8533991b10b5
lies046 <skytree.skysky@gmail.com>
2020-03-26 15:01:42 +0900

    Add test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get abput" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
 end
